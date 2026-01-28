b1ddae8247c82ee2db7c5e40a017d09423ccdd8c
Sam Hibberd <samhibberd@unknown72dccb98ae40.lan>
2026-01-28 12:42:12 +0000

    Fix Astro config for GitHub Pages (no adapter)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import staticAdapter from "@astrojs/static";
 
 export default defineConfig({
   output: "static",
-  adapter: staticAdapter(),
   site: "https://hibberds100.github.io",
   base: "/website-samsara",
 });
